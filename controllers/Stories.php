1e1a13054acedecf11a340fbdc1928631719037c
meysam mahfouzi      <mmahfouzi@wayfair.com>
2020-05-23 20:18:22 +0200

    Add record handler to stories list

--- controllers/Stories.php
+++ controllers/Stories.php
@@ -2,6 +2,7 @@
 
 use Backend\Classes\Controller;
 use Backend\Facades\BackendMenu;
+use Meysam\Test\Models\Story as StoryModel;
 
 class Stories extends Controller
 {
@@ -19,5 +20,22 @@ class Stories extends Controller
     {
         parent::__construct();
         BackendMenu::setContext('Meysam.Test', 'main-menu-test', 'side-menu-stories');
+    }
+
+    public function onLoadWordForm()
+    {
+        $config            = $this->makeConfig('$/meysam/test/models/story/fields_short.yaml');
+        $config->alias     = '';
+        $config->arrayName = '';
+        $config->model     = StoryModel::find(post('record_id'));
+        $storyFormWidget   = $this->makeFormWidget('Backend\Widgets\Form', $config);
+        $storyFormWidget->bindToController();
+
+        $this->initForm($config->model);
+
+        $this->vars['storyFormWidget'] = $storyFormWidget;
+        $this->vars['recordId'] = post('record_id');
+
+        return $this->makePartial('story_form');
     }
 }
